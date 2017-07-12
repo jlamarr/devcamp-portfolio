0634f1c096fd90846b9e7d510ec1bce01cc926a8
Jordan Hudgens <jordan@devcamp.com>
2017-07-12 17:47:34 +0000

    my message

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'pages/about'
 
   get 'pages/contact'
-
+  root to: 'pages#home'
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
