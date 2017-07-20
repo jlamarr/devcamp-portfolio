f0d5ddc3789ad855ba1cb5091c453efc25e06945
Jordan Hudgens <jordan@devcamp.com>
2017-07-20 16:07:23 +0000

    custom reg routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path_names: { sign_up: '' }
   resources :users
   resources :portfolios, except: [:show]
   get 'angular-items', to: 'portfolios#angular'
